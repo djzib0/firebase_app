e6bc7e2faf8417740837c53764565ef7d3852bcc
djzib0 <piotrsabatura@gmail.com>
2023-04-20 19:10:20 +0200

    setting form

--- shopping_list_app/src/components/Form.jsx
+++ shopping_list_app/src/components/Form.jsx
@@ -1,15 +1,33 @@
 import React, { useState } from "react";
 
 export default function Form() {
-    const [formData, setFormData] = useState({
-        cart: ""
-    })
+    const [formData, setFormData] = useState(
+        {cart: ""}
+    )
+
+    function handleChange(e) {
+        const {name, value} = e.target
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [name]: value
+            }
+        })
+    }
+
+    console.log("newFormData", formData)
 
     return (
         <section>
             <div className="container form-container">
                 <h1>Tutaj fotka kotka</h1>
-                <input type="text" id="input-field" placeholder="Bread" />
+                <input type="text" 
+                       name="cart" 
+                       id="input-field" 
+                       placeholder="Bread"
+                       onChange={handleChange}
+                       value={formData.cart}
+                    />
                 <button id="add-button">Add to cart</button>
             </div>
         </section>
